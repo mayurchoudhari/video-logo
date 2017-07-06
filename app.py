7abefc04bbded9ee6da50b54706058fccfa01118
Mayur <mayur@techxtrem.com>
2017-07-06 13:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from moviepy.editor import *
 
-video = VideoFileClip("input.mp4")
+video = VideoFileClip("input.mp4") # input video file, same folder as app.py
 W,H = video.size
 
+# Input logo, preferable png with no background
 logo = (ImageClip("logo.png")
           .set_duration(video.duration)
           .resize(height=H/5) # if you need to resize...
-          .margin(right=int(W/20), top=int(H/20), opacity=0) # (optional) logo-border padding
+          .margin(right=int(W/20), top=int(H/20), opacity=0) # position of logo
           .set_pos(("right","top")))
 
 final = CompositeVideoClip([video,logo])
-final.write_videofile("output.mp4")
+final.write_videofile("output.mp4") # output file
